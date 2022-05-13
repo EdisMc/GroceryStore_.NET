baaa71152f2a370b4a5fa2e2a0d7de6a45d98574
Edis <edy_l20@abv.bh>
2022-05-13 21:03:33 +0300

    Add project files.

---
+++ TSP_Project_2022_Supermarket/Splash.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TSP_Project_2022_Supermarket
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Progress = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(77, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(513, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Grocery Store Management System";
+            // 
+            // Progress
+            // 
+            this.Progress.FillColor = System.Drawing.Color.Maroon;
+            this.Progress.ForeColor = System.Drawing.Color.Maroon;
+            this.Progress.Location = new System.Drawing.Point(-1, 288);
+            this.Progress.Name = "Progress";
+            this.Progress.ProgressColor = System.Drawing.Color.White;
+            this.Progress.ProgressColor2 = System.Drawing.Color.White;
+            this.Progress.Size = new System.Drawing.Size(671, 13);
+            this.Progress.TabIndex = 5;
+            this.Progress.Text = "guna2ProgressBar1";
+            this.Progress.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.Progress.ValueChanged += new System.EventHandler(this.guna2ProgressBar1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(285, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(673, 299);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            this.Load += new System.EventHandler(this.Splash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ProgressBar Progress;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ TSP_Project_2022_Supermarket/Splash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSP_Project_2022_Supermarket
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ProgressBar1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        int start_point = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            start_point++;
+            Progress.Value = start_point;
+
+            if (Progress.Value == 100)
+            {
+                Progress.Value = 0;
+                timer1.Stop();
+                Form1 form1 = new Form1();
+                this.Hide();
+                form1.Show();
+            }
+
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
